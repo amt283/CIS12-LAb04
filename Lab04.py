9856f3d6c70a956cbdd262f90bef5e4b29eb5a9c
amt954 <athompson283@gmail.com>
2025-02-15 12:58:39 -0800

    Initial commit

---
+++ Lab04.py
@@ -0,0 +1,24 @@
+import turtle
+
+"""PUT YOUR FUNCTIONS HERE"""
+
+# Create a turtle object
+t = turtle.Turtle()
+
+# Hide the turtle and set speed
+t.speed(10)  # 1 is slow, 10 is fast, 0 is instant
+t.hideturtle()
+
+# Create a window to draw in
+# Create a new turtle screen and set its background color
+screen = turtle.Screen()
+screen.bgcolor("darkblue")
+# Set the width and height of the screen
+screen.setup(width=600, height=600)
+# Clear the screen
+t.clear()
+
+"""PUT YOUR DRAW CALLS TO FUNCTIONS HERE"""
+
+# Close the turtle graphics window when clicked
+turtle.exitonclick()
